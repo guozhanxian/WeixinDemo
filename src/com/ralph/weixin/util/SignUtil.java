fa071387af50b3711135647e00e61e1b2c4fbe44
guozhanxian <guozhanxian@gmail.com>
2016-06-30 16:11:22 +0800

    蝈蝈提交

--- src/com/ralph/weixin/util/SignUtil.java
+++ src/com/ralph/weixin/util/SignUtil.java
@@ -1,65 +1,84 @@
 package com.ralph.weixin.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 /**
- * Created by Ralph on 2016/6/28.
+ * 绑定服务器工具类
+ *
+ * @author peiyu
  */
-public class SignUtil
-{
-    private static String token = "guozhanxian";
+public final class SignUtil {
 
-    public static boolean checkSignature(String signature, String timestamp, String nonce){
+    private static final Logger LOG   = LoggerFactory.getLogger(SignUtil.class);
+    private static final char[] digit = {'0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    /**
+     * 此类不需要实例化
+     */
+    private SignUtil() {
+    }
+
+    /**
+     * 认证微信，可以参见微信开发者文档
+     *
+     * @param token     我们自己设定的token值
+     * @param signature 微信传来的变量
+     * @param timestamp 微信传来的变量
+     * @param nonce     微信传来的变量
+     * @return 是否合法
+     */
+    public static boolean checkSignature(String token, String signature,
+                                         String timestamp, String nonce) {
+        if (StrUtil.hasBlank(token, signature, timestamp, nonce)) {
+            return false;
+        }
         String[] arr = new String[]{token, timestamp, nonce};
-        // 将 token, timestamp, nonce 三个参数进行字典排序
         Arrays.sort(arr);
         StringBuilder content = new StringBuilder();
-        for(int i = 0; i < arr.length; i++){
-            content.append(arr[i]);
+        for (String anArr : arr) {
+            content.append(anArr);
         }
-        MessageDigest md = null;
+        MessageDigest md;
         String tmpStr = null;
 
         try {
             md = MessageDigest.getInstance("SHA-1");
-            // 将三个参数字符串拼接成一个字符串进行 shal 加密
-            byte[] digest = md.digest(content.toString().getBytes());
+            byte[] digest = md.digest(content.toString().getBytes("UTF-8"));
             tmpStr = byteToStr(digest);
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            LOG.error("加密方式异常", e);
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("编码格式不支持", e);
         }
-        content = null;
-        // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
-        return tmpStr != null ? tmpStr.equals(signature.toUpperCase()): false;
+        return tmpStr != null && tmpStr.equalsIgnoreCase(signature);
     }
 
     /**
-     * 将字节数组转换为十六进制字符串
-     * @param digest
-     * @return
+     * 将byte数组变为16进制对应的字符串
+     *
+     * @param byteArray byte数组
+     * @return 转换结果
      */
-    private static String byteToStr(byte[] digest) {
-        String strDigest = "";
-        for(int i = 0; i < digest.length; i++){
-            strDigest += byteToHexStr(digest[i]);
+    private static String byteToStr(byte[] byteArray) {
+        int len = byteArray.length;
+        StringBuilder strDigest = new StringBuilder(len * 2);
+        for (byte aByteArray : byteArray) {
+            strDigest.append(byteToHexStr(aByteArray));
         }
-        return strDigest;
+        return strDigest.toString();
     }
 
-    /**
-     * 将字节转换为十六进制字符串
-     * @param b
-     * @return
-     */
-    private static String byteToHexStr(byte b) {
-        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    private static String byteToHexStr(byte mByte) {
         char[] tempArr = new char[2];
-        tempArr[0] = Digit[(b >>> 4) & 0X0F];
-        tempArr[1] = Digit[b & 0X0F];
-
-        String s = new String(tempArr);
-        return s;
+        tempArr[0] = digit[(mByte >>> 4) & 0X0F];
+        tempArr[1] = digit[mByte & 0X0F];
+        return new String(tempArr);
     }
 }
